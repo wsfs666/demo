98bd4d1538cec18ebedb023550e194bae2a9330a
wsfs666 <60485225+wsfs666@users.noreply.github.com>
2022-12-07 07:50:47 -0800

    Delete src directory

--- src/main/java/com/example/university/respository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.university.respository;
-
-import com.example.university.domain.entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher,String>, TeacherCustomRepo{
-}
