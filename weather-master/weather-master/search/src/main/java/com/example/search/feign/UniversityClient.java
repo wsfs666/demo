21aaea35de0af8edf6b17a7d2fbb3544ee8c06cd
wsfs666 <60485225+wsfs666@users.noreply.github.com>
2022-12-12 08:01:00 -0800

    Update UniversityClient.java

--- weather-master/weather-master/search/src/main/java/com/example/search/feign/UniversityClient.java
+++ weather-master/weather-master/search/src/main/java/com/example/search/feign/UniversityClient.java
@@ -5,10 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @author 舒望
- * @date 2022/12/12 11:29
- */
+
 @FeignClient(value = "university-service")
 public interface UniversityClient {
 
